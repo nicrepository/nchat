0818c4c4476822231f6fa1165010847a3d0e0d4d
k4neca <caiocsa98@gmail.com>
2026-06-03 10:26:11 -0300

    test(web): add basic auth e2e tests

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
+    exclude: ["**/node_modules/**", "**/e2e/**"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
